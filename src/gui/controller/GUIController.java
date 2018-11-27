a5c82db784507aef0566487836e0bfcb01f20038
Alex Rottlaender <AlexRottlaender@gmail.com>
2018-11-27 11:54:49 -0700

    created start method and main method and a constant method

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -2,5 +2,21 @@ package gui.controller;
 
 public class GUIController
 {
+	public GUIController()
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+	}
+	
+	public int returnConstant(int number, int repetitions)
+	{
+		int returnValue = 0;
+		
+		return returnValue;
+	}
 
 }
